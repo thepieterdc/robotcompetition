d617c6d7ffb9ebb850eef80e571321455af10d67
Pieter De Clercq <pieterdeclercq@outlook.com>
2016-04-19 21:24:52 +0200

    Jeej

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <dwengoConfig.h>
 #include <dwengoBoard.h>
 #include <dwengoADC.h>
-#include <dwengoLCD.h>
 #include <dwengoMotor.h>
+
+int max(int x, int y);
 
 int V = 0;
 int RA = 1;
@@ -14,63 +15,97 @@ int rechtsAchter = 0;
 
 int vinnig = 0;
 int gem = 0;
+int andereMotor = 0;
 
 void main(void) {
     initBoard();
-    initLCD();
     initADC();
     initMotor();
-    clearLCD();
-   
 
-    backlightOn();
 
     while (SW_C != PRESSED) {
         delay_ms(50);
     }
-    
 
+    LEDS = 0b00000101;
+    backlightOn();
+    delay_s(1);
+    LEDS = 0b00000100;
+    backlightOff();
+    delay_s(1);
+    LEDS = 0b00000011;
+    backlightOn();
+    delay_s(1);
+    LEDS = 0b00000010;
+    backlightOff();
+    delay_s(1);
+    LEDS = 0b00000001;
+    backlightOn();
+    delay_s(1);
+
+
+    rechtsVoor = readADC(RV);
+    rechtsAchter = readADC(RA);
+    gem = (rechtsVoor + rechtsAchter) / 2;
+
+    /*
+     * TODO: wat dan ook
+    if (gem < 200) {
+        setSpeedMotor1(700);
+        setSpeedMotor2(1023);
+        delay_ms(700);
+        setSpeedMotor1(1023);
+        setSpeedMotor2(1023);
+        while (max(rechtsVoor, rechtsAchter) < 200) {
+            rechtsVoor = readADC(RV);
+            rechtsAchter = readADC(RA);
+            delay_ms(18);
+        }
+    backlightOff();
+
+    }
+     */
 
     //LINKS = 2 RECHTS = 1
     while (TRUE) {
-        while (SW_S == PRESSED) {
-            delay_ms(50);
-            return;
-        }
         voor = readADC(V);
         rechtsVoor = readADC(RV);
         rechtsAchter = readADC(RA);
-        
+
         vinnig = 7;
-        
-        gem = (rechtsVoor+rechtsAchter)/2;
-        
+
+        gem = (rechtsVoor + rechtsAchter) / 2;
+
         //Als dicht bij juiste muur en vijand
-        if( voor > 500 && gem > 150 && voor < 600){
+        if (voor > 500 && gem > 150 && voor < 600) {
             voor = 3.1415926535;
         }
-        
-        clearLCD();
-        printIntToLCD(rechtsVoor, 0, 0);
-        printIntToLCD(rechtsAchter, 1, 1);
-        
-        if(voor > 500) {
+
+        andereMotor = vinnig * (rechtsAchter - rechtsVoor);
+
+        if (voor > 470) {
+            backlightOn();
             setSpeedMotor1(700);
             setSpeedMotor2(1023);
             delay_ms(300);
-        } else if(gem > 350) {
+            backlightOff();
+        } else if (gem > 350) {
             setSpeedMotor1(1023);
             setSpeedMotor2(700);
-        } else if(rechtsAchter > (rechtsVoor * 1.2)) {
-            setSpeedMotor1(1023-vinnig*(rechtsAchter-rechtsVoor));
+        } else if (rechtsAchter > (rechtsVoor * 1.2)) {
+            setSpeedMotor1(1023 - andereMotor);
             setSpeedMotor2(1023);
-        } else if(rechtsAchter * 1.2 < rechtsVoor) {
+        } else if (rechtsAchter * 1.2 < rechtsVoor) {
             setSpeedMotor1(1023);
-            setSpeedMotor2(1023-vinnig*(rechtsAchter-rechtsVoor));
+            setSpeedMotor2(1023 - andereMotor);
         } else {
             setSpeedMotor1(1023);
             setSpeedMotor2(1023);
         }
         delay_ms(3);
     }
+}
+
+int max(int x, int y) {
+    return x ^ ((x ^ y) & -(x < y));
 }
