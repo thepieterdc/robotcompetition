f3b637692e1184fb623fadb9a70d85d189270607
Pieter De Clercq <pieterdeclercq@outlook.com>
2017-05-02 20:47:26 +0200

    Woo

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=dirk.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/dirk.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=dirk.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/dirk.x.tar
--- sensortest.c
+++ sensortest.c
@@ -10,16 +10,81 @@
 #include <dwengoLCD.h>
 #include <dwengoMotor.h>
 
+#define MOTOR_LINKS setSpeedMotor2
+#define MOTOR_RECHTS setSpeedMotor1
+
+#define GROND_VOOR 2
+#define GROND_ACHTER 1
+#define VER_LINKS 0
+#define VER_RECHTS 3
+
+void vooruit(int spd);
+
+void achteruit(int spd);
+
+void links_terplekke(int spd);
+
+void links_vooruit(int spd);
+
+void rechts_terplekke(int spd);
+
+void rechts_vooruit(int spd);
+
+int sensor(int adc);
+
+void stop();
+    
 int main(int argc, char** argv) {
     initBoard();
     backlightOn();
-    initLCD();
+    initMotor();
     initADC();
+    initLCD();
     
     while(1) {
-        clearLCD();
-        printIntToLCD(readADC(0), 0, 0);
-        printIntToLCD(readADC(1), 1, 1);
-        delay_ms(500);
+        if(sensor(GROND_VOOR) > 250 && sensor(GROND_ACHTER) < 250) {
+            vooruit(1023);
+        } if(sensor(GROND_VOOR) < 250 && sensor(GROND_ACHTER) > 250) {
+            achteruit(1023);
+        }
     }
+}
+
+void vooruit(int spd) {
+    MOTOR_LINKS(spd);
+    MOTOR_RECHTS(spd);
+}
+
+void achteruit(int spd) {
+    MOTOR_LINKS(-spd);
+    MOTOR_RECHTS(-spd);
+}
+
+void links_terplekke(int spd) {
+    MOTOR_LINKS(spd);
+    MOTOR_RECHTS(-spd);
+}
+
+void links_vooruit(int spd) {
+    MOTOR_LINKS(spd);
+    MOTOR_RECHTS(spd*2/3);
+}
+
+void rechts_terplekke(int spd) {
+    MOTOR_LINKS(-spd);
+    MOTOR_RECHTS(spd);
+}
+
+void rechts_vooruit(int spd) {
+    MOTOR_LINKS(spd*2/3);
+    MOTOR_RECHTS(spd);
+}
+
+int sensor(int adc) {
+    return readADC(adc);
+}
+
+void stop() {
+    MOTOR_LINKS(0);
+    MOTOR_RECHTS(0);
 }
